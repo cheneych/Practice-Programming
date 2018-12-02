b09fe7828413047a317f10cb79cc4d7ed449b39b
Weerdhawal <wcmb3@mst.edu>
2018-12-01 20:39:01 -0800

    feverDetector added

---
+++ src/cerner/patient/NurseNotifications.java
@@ -0,0 +1,7 @@
+package cerner.patient;
+
+import java.util.List;
+
+public class NurseNotifications {
+	private List<PatientSchedule> nurseSchedule;
+}
--- src/cerner/temperature/FeverDetector.java
+++ src/cerner/temperature/FeverDetector.java
@@ -8,6 +8,8 @@ public class FeverDetector {
 	
 	public void addPatientTemperature(Temperature temperature) {
 		this.rear++;
+		if(this.front==-1||front>=rear)
+					this.front++;
 		if(rear>temperaturePerDay.size()) {
 			this.rear=0;
 			this.front++;
@@ -16,4 +18,44 @@ public class FeverDetector {
 		
 	}
 	
+	public Temperature getTemperature24Hours() {
+		if(front==-1) {
+			return null;
+		}
+		return this.temperaturePerDay.get(this.front);
+	}
+	public Temperature getMostRecentTemperature() {
+		if(this.rear!=-1) {
+			return temperaturePerDay.get(this.rear);
+			
+		}
+		return null;
+	}
+	
+	public Boolean hasFever() {
+		if(this.temperaturePerDay.get(this.rear).getTemperatureInFahrenheit() >98.6 ) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void printValues() {
+		if(this.rear>this.front) {
+			for(int i=this.front;i<=this.rear;i++) {
+				System.out.println(this.temperaturePerDay.get(i).getTemperature());
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		FeverDetector fd=new FeverDetector();
+		fd.addPatientTemperature(new Temperature((float) 98.6, Location.ARMPIT));
+		fd.addPatientTemperature(new Temperature((float) 98.6, Location.ARMPIT));
+		fd.addPatientTemperature(new Temperature((float) 98.6, Location.ARMPIT));		
+		fd.addPatientTemperature(new Temperature((float) 98.6, Location.ARMPIT));
+		fd.addPatientTemperature(new Temperature((float) 98.6, Location.ARMPIT));
+		fd.addPatientTemperature(new Temperature((float) 98.6, Location.ARMPIT));
+		
+		fd.printValues();
+	}
 }
